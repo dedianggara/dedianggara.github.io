f94ed8debac2bcf36354349d1dcb15cac85f2e83
dedianggara <dedianggara221@gmail.com>
2023-08-20 22:48:15 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        
+        primary: '#025464',
+        secondary: '#d4d4d4',
+        dark: '#0f172a',
+        background : '#001C30',
+        background2 : '#C4D7B2',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
